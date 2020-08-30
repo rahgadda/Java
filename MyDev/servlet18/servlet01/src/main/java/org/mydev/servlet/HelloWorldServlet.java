8ceaabd3ef3f4e1436e07d53240d77bff9d78feb
rahgadda <rahgadda@RAHGADDA-IN.in.oracle.com>
2020-08-30 05:42:18 +0530

    Initial Draft

--- MyDev/servlet18/servlet01/src/main/java/org/mydev/servlet/HelloWorldServlet.java
+++ MyDev/servlet18/servlet01/src/main/java/org/mydev/servlet/HelloWorldServlet.java
@@ -11,12 +11,13 @@ public class HelloWorldServlet extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException,IOException
     {
-        //setting the content type
+        //Setting the content type
         res.setContentType("text/html");
-        //get the stream to write the data
+
+        //Get the stream to write the data
         PrintWriter pw=res.getWriter();
 
-        //writing html in the stream
+        //Writing html in the stream
         pw.println("Hello,Welcome to Servlet Tutorial World");
     }
 }
